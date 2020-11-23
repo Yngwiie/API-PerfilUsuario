e096f5fab67912b42b39ed4ada9552e18dfd29b5
Javier Aliaga <reivaj_31@hotmail.com>
2020-11-23 18:04:40 -0300

    Implementación API de autenticación.
    Se implementa los endpoints /login, /register y /me.
    
    1) /login: se utiliza para validar los datos al iniciar sesión, en donde se genera un token de autenticación.
    
    2) /register: endpoint para registrar los datos de un nuevo usuario, validando que cumpla los atributos necesarios tales como, nombre, email y contraseña.
    
    3) /me: endpoint para obtener los datos del usuario que esta autenticado, por lo que para poder acceder se debe tener el token que se obtiene al estar logeado.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{   
+     /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login','register']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $validated_data = Validator::make(
+            $request->all(),
+            [
+                'email' => 'required|email',
+                'password' => 'required|string|min:8',
+            ]
+        );
+        
+        if($validated_data->fails()){
+            return response()->json($validated_data->errors(),400);
+        }
+        
+        $credentials = request(['email', 'password']);
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+    /**
+     * 
+     */
+    public function register(Request $request)
+    {
+        $validated_data = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|string|min:2|max:100',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed|string|min:8',
+            ]
+        ); 
+
+        if($validated_data->fails()){
+            return response()->json($validated_data->errors(),422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json(['message' => '¡User created sucessfully!','user' => $user]);
+
+    }
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function profile()
+    {   
+        return response()->json(auth()->user());
+    }
+    
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+}); */
+Route::group([
+    'middleware' => 'api',
+    'namespace' => 'App\http\Controllers',
+    'prefix' => 'auth'
+],function($router){
+    Route::post('login','AuthController@login'); 
+    Route::get('me','AuthController@profile'); 
+    Route::post('new','AuthController@register'); 
 });
